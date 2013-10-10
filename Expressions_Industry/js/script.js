939c0cd8609cd31472fe59b67f1c18620116a4e1
rarjune <graphicsflo@Rajmaties-MacBook-Pro.local>
2013-10-10 16:19:07 -0400

    update

---
+++ Expressions_Industry/js/script.js
@@ -0,0 +1,6 @@
+var size 8.5in 11.0in;
+var margin 1.0 1.25in 1.25in;
+var header margin .5;
+var footer margin .5;
+console.log (size margins);
+
---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,6 @@
+// var Rajmatie Arjune October 10, 2013 Expressions_Personal
+var hair color = 12.99;
+var needed per year = 4;
+var hair color * needed per year;
+
+
