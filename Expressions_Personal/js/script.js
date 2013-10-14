80b858262f246a22e0edf01ebccee3045913dc1f
josephdalton <jdalton@fullsail.com>
2013-10-14 15:26:08 -0400

    JMD

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,5 +1,7 @@
 // var Rajmatie Arjune October 10, 2013 Expressions_Personal
+
+//Did you test this?
 var haircolor = 12.99;
 var needed per year = 4;
 var haircolor = needed per year;
-console.log ("haircolor * needed per year");
+console.log ("haircolor * needed per year"); 
